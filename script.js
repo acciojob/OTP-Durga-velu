12ae3fa4568b6aa23ace4c999809e06e5610cbcf
Durga-velu <durgadevi134babu@gmail.com>
2025-04-15 22:46:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your JS code here. If required.
+
+
+const inputvalue=document.getElementsByClassName("code")
+
+
+   
+
+    inputvalue.forEach((ele) => {
+         
+        ele.addEventListener(("keyup"),(e)=>{
+
+            let currentEle=e.target;
+            let inputField=e.key.charCodeAt(0)
+            console.log(e.key);
+            
+
+            if(e.key=="Backspace"){
+               let preElement=currentEle.previousElementSibling;
+               if(preElement){
+                preElement.focus()
+               } 
+               return;
+            }
+   
+
+       let nextEle=currentEle.nextElementSibling;
+       if(nextEle){
+        nextEle.focus()
+       }
+       else{
+              e.target.value="";
+       }
+    });
+    });
+
+
+
+
+
+
+
