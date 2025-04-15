48d6ca89f826177742053d17e148a6bb4b9d7ee0
Durga-velu <durgadevi134babu@gmail.com>
2025-04-15 23:16:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,20 +5,16 @@
 const inputvalue=document.querySelectorAll(".code")
 
 
-   
-
-    inputvalue.forEach((ele,index) => {
-         
+       inputvalue.forEach((ele,index) => {   
         ele.addEventListener(("keyup"),(e)=>{
-
             let currentEle=e.target.value;
-           
+ if(e.key>=0 && e.key <=9){
             if(currentEle.length==1 && index<inputvalue.length-1){
                
-              inputvalue[index+1].focus();
-
-            }
-        });
+                inputvalue[index+1].focus();
+  
+              }
+           }        });
 
        ele.addEventListener(("keydown"),(e)=>{
 
